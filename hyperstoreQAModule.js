9b111d3308d901a41907b1c69cb5c3a6379410c4
Edward Miller <sewerbird@gmail.com>
2014-06-15 19:15:41 +0800

    Initial commit

---
+++ hyperstoreQAModule.js
@@ -0,0 +1,65 @@
+//Hyperstore Q&A Module
+
+/**
+* @jsx React.DOM
+*/
+
+function HyperstoreQAModule(domTargetID, hyperstoreApplicationName){
+	/*
+		QA Module
+			Topic Statement
+				Contributor Info (Asker's)
+					User Name
+				Topic Question
+					editable h3
+				Topic Details
+					Text area with markdown
+				Comments
+
+				Comment Submit
+				Topic Status
+			Answer Submit
+				Contributor Info (User's)
+				Submission Field
+			Answer
+				Contributor Info (Answeror's)
+				Up/downvote Widget
+				Answer Text
+				Comments
+				Comment Submit
+	*/
+
+	//QA Module
+	var QAModule = React.createClass({displayName:"QAModule",
+		getInitialState: function(){
+
+		},
+		render: function(){
+
+		},
+		handleAnswerSubmit: function(){
+
+		}
+	})
+
+	//Topic Statement
+	var QATopic = React.createClass({displayName:"TopicStatement",
+		render: function(){
+
+		}
+	})
+
+	//Answer Submit
+	var QASubmitAnswer = React.createClass({displayName:"AnswerSubmit",
+		render: function(){
+
+		}
+	})
+
+	//Answer
+	var QAAnswer = React.createClass({displayName:"Answer",
+		render: function(){
+			
+		}
+	})
+}
